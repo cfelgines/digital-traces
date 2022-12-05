4a7b438e72e067869977b823c6fc930cae016c79
cfelgines <chloe.felgines@epfedu.fr>
2022-12-05 14:55:34 +0100

    google analytics

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def hello_world():
 def hello_world():
     prefix_google = """
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script> 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PZD5P4M93D"></script> 
     <script>
     window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
-    gtag('config', ' YOUR_GA_CODE'); </script>
+    gtag('config', 'G-PZD5P4M93D'); </script>
     """
     return prefix_google + "Hello World"
