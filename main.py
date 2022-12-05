231cddbfad7f22c655cd93843ee16a99871be7fe
cfelgines <chloe.felgines@epfedu.fr>
2022-12-05 14:49:39 +0100

    google analytics add

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def hello_world():
     return "Hello World"
+
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script> 
+    <script>
+    window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
+    gtag('config', ' YOUR_GA_CODE'); </script>
+    """
+    return prefix_google + "Hello World"
